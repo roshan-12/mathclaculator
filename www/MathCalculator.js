2cd209511df979e699c507c9cc51ffb5454e7c16
alethea <alethea@Altheas-Mac-mini.local>
2019-01-28 13:23:26 +0530

    Initial commit

---
+++ www/MathCalculator.js
@@ -0,0 +1,21 @@
+var exec = require('cordova/exec');
+
+module.exports.coolMethod = function (arg0, success, error) {
+    exec(success, error, 'Calculator', 'coolMethod', [arg0]);
+};
+
+module.exports.add = function (arg0, success, error) {
+    exec(success, error, 'Calculator', 'add', [arg0]);
+};
+
+module.exports.multiply = function (arg0, success, error) {
+    exec(success, error, 'Calculator', 'multiply', [arg0]);
+};
+
+module.exports.substract = function (arg0, success, error) {
+    exec(success, error, 'Calculator', 'substract', [arg0]);
+};
+
+module.exports.divide = function (arg0, success, error) {
+    exec(success, error, 'Calculator', 'divide', [arg0]);
+};
